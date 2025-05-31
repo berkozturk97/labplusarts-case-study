9cd22240a6fd590ee7be0e8c721d740d6ddd9a0e
berkozturk97 <berk1ozturk1@gmail.com>
2025-05-31 16:42:32 +0300

    chore(style): add global styles

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      // CSS Modules için klasör ve dosya isimlendirme
+      generateScopedName: "[name]__[local]___[hash:base64:5]",
+      // Global CSS değişkenlerinin çalışması için
+      globalModulePaths: [/\.global\.(css|scss|sass)$/],
+    },
+  },
 });
